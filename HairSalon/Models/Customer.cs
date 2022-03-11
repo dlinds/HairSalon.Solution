84c6665efab22ba0d129a499b75c6628a9d810ba
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-03-11 09:45:37 -0800

    build out constructors for stylist and customer

--- HairSalon/Models/Customer.cs
+++ HairSalon/Models/Customer.cs
@@ -1,6 +1,14 @@
+using System;
+
 namespace HairSalon.Models
 {
   public class Customer
   {
+    public int CustomerId { get; set; }
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public string PreferredCut { get; set; }
+    public int TimesVisited { get; set; }
+    public DateTime Birthday { get; set; }
   }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,6 +1,24 @@
+using System.Collections.Generic;
+using System;
+
 namespace HairSalon.Models
 {
   public class Stylist
   {
+    public Stylist()
+    {
+      this.Customers = new HashSet<Customer>();
+    }
+
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public DateTime HireDate { get; set; }
+    public string Specialty { get; set; }
+    public int PriorExperienceInYears { get; set; }
+    public string ScheduledDays { get; set; }
+    public TimeSpan ShiftStart { get; set; }
+    public TimeSpan ShiftEnd { get; set; }
+
+    public virtual ICollection<Customer> Customers { get; set; }
   }
 }
