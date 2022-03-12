9df5d65c2b9431d1bb51cb48831cccd46436556f
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-03-11 22:44:05 -0800

    enable form on home page, update bootstrap to 5.0

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -9,10 +9,18 @@ namespace HairSalon.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly HairSalonContext _db;
+
+    public HomeController(HairSalonContext db)
+    {
+      _db = db;
+    }
 
     [HttpGet("/")]
     public ActionResult Index()
     {
+      // List<Stylist> stylists = new List<Stylist> { };
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "FirstName");
       ViewBag.PageName = "Home";
       return View();
     }
